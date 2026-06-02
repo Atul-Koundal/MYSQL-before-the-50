57e8a2c02a8c48b1974c2b334ea33ef80a877d0b
Atul-Koundal <24bcs031@nith.ac.in>
2026-06-02 19:12:18 +0530

    Add files via upload

---
+++ where_clause.sql
@@ -0,0 +1,48 @@
+SELECT *
+FROM employee_salary
+WHERE first_name = 'Leslie'
+;
+
+SELECT *
+FROM employee_salary
+WHERE salary > 50000
+;
+
+SELECT * 
+FROM employee_demographics
+WHERE birth_date > '1985-01-01'
+;
+
+SELECT *
+FROM employee_salary
+WHERE birth_date > '1985-01-01'
+AND gender = 'male'
+;
+
+SELECT *
+FROM employee_demographics
+WHERE (first_name = 'Leslie' AND age = 44) OR age > 55
+;
+
+SELECT * 
+FROM employee_demographics
+WHERE first_name LIKE 'Jer%'
+;
+
+SELECT * 
+FROM employee_demographics
+WHERE first_name LIKE 'Jer__'
+;
+
+SELECT * 
+FROM employee_demographics
+WHERE first_name LIKE 'a__'
+;
+
+SELECT * 
+FROM employee_demographics
+WHERE first_name LIKE 'a__%'
+;
+
+
+
